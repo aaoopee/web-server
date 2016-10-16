5721b5cd469ae9ec265599e385e06a98794bbb1e
aaoopee <antti.pelkonen@iki.fi>
2016-10-16 16:57:34 +0300

    Initialize repository

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var PORT = 3000;
+var app = express();
+var middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req,res,next) {
+		var date = new Date().toString();
+		console.log('Request: '+date+' '+req.method+' '+req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+
+// app.get('/', function(req, resp) {
+// 	resp.send('Hello Express!');
+// });
+
+app.get('/about', middleware.requireAuthentication, function(req, resp) {
+	resp.send('About us!')
+});
+
+app.use(express.static(__dirname+'/public'));
+
+app.listen(PORT, function() {
+	console.log('Server started at port '+PORT+'.');
+});
